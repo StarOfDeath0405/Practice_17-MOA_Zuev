b100d7d57780f80c1486bbdd7fb043bfddcae0dc
StarOfDeath0405 <zuev-888@mail.ru>
2020-07-18 07:53:05 +0300

    Add files via upload

---
+++ APP1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(155-30);
+            a = Int32.Parse(Console.ReadLine());
+            b = Int32.Parse(Console.ReadLine());
+            c = a + b;
+            Console.Write(c);
+        }
+
+    }
+}
